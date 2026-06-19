3907ece9cd1b6d2a1f9599814d4479af820f6b06
Tina Gunnarsson <roundbrackets@gmail.com>
2026-06-19 16:27:26 -0700

    blah

---
+++ client/client_test.go
@@ -0,0 +1,29 @@
+package client
+
+import (
+	"testing"
+)
+
+/*
+func TestKubeconfig(t *testing.T) {
+	t.Error("intentional error 1")
+}
+
+func TestBar(t *testing.T) {
+	result := Bar()
+	if result != "bar" {
+		t.Errorf("expecting bar, got %s", result)
+	}
+}
+
+func TestQuz(t *testing.T) {
+	t.Error("intentional error 2")
+}
+	*/
+
+/*
+26:func (c *ClientOpts) Kubeconfig () (string, error) {
+42:func CommandLineClient (clientOpts ClientOpts) (clientset *kubernetes.Clientset, err error) {
+64:func InClusterClient (clientOpts ClientOpts) (clientset *kubernetes.Clientset, err error) {
+76:func MakeClient (clientOpts ClientOpts) (client *Client, err error) {
+*/
---
+++ threads/main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	//"strings"
+	//	"time"
+
+	c "github.com/roundbrackets/cluster-gossip/client"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+type Event struct {
+	namespace string
+	podCnt    int
+	eventCnt  int
+	events    []string
+}
+
+var (
+	Namespaces []string
+)
+
+func main() {
+	clien, err := c.MakeClient(c.ClientOpts{ClientType: c.CommandLine})
+	if err != nil {
+		fmt.Printf("Error encountered: %v\n", err)
+		os.Exit(1)
+	}
+
+	ns := namespaces(client)
+	for n := range ns {
+		n.ToString()
+	}
+}
+
+// we want it to listen to namespaces
+
+func (e *Event) ToString() {
+	fmt.Printf("%-30s %3d %3d\n", e.namespace, e.podCnt, e.eventCnt)
+
+	for _, e := range e.events {
+		fmt.Printf("  %s\n", e)
+	}
+}
+
+func namespaces(client *c.Client) <-chan *Event {
+	out := make(chan *Event)
+
+	ns, err := client.Namespaces().List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		fmt.Printf("Error encountered: %v\n", err)
+		return out
+	}
+
+	go func() {
+		for _, n := range ns.Items {
+			event := &Event{namespace: n.Name}
+			event.podCnt = pods(client, n.Name)
+			event.events, event.eventCnt = events(client, n.Name)
+			out <- event
+		}
+		close(out)
+	}()
+
+	return out
+}
+
+func pods(client *c.Client, namespace string) int {
+	pods, err := client.Pods(namespace).List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		fmt.Printf("Error encountered: %v\n", err)
+		return -1
+	}
+
+	return len(pods.Items)
+}
+
+func events(client *c.Client, namespace string) ([]string, int) {
+	events, err := client.Events(namespace).List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		fmt.Printf("  Error encountered: %v\n", err)
+		return nil, -1
+	}
+
+	e := []string{}
+	for _, event := range events.Items {
+		e = append(e, event.Message)
+	}
+
+	return e, len(events.Items)
+
+	/*
+		fmt.Println("\nEvents\n------");
+		events, err := client.Events(namespace.Name).List(context.TODO(), metav1.ListOptions{})
+		if err != nil {
+			fmt.Printf("  Error encountered: %v\n", err)
+			return
+		}
+		for _, event := range events.Items {
+			fmt.Printf("  %s\n", event.Message);
+			fmt.Printf("    %-30s %-30s %-30s\n", event.Reason, event.Type, event.Action);
+			fmt.Printf("    %-30s %-30s\n", event.FirstTimestamp, event.LastTimestamp);
+			// EventTime, Series
+		}
+
+		fmt.Printf("  There are %d pods in the %s namespace\n", len(events.Items), namespace.Name)
+	*/
+}
